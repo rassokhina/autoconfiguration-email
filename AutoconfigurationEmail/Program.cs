0c25eaf8f733c9d69ddd7246f1c8263e4cf85fed
Anastasia Rassokhina <arassohina@themsteam.com>
2018-10-24 18:34:40 +0700

    Add project files.

---
+++ AutoconfigurationEmail/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoconfigurationEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
